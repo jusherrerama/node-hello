ea03f8fd1a8d89713875d8e4aef4a6bf82e712b5
Juan Herrera <jusherrerama@unal.edu.co>
2021-06-02 23:15:17 -0500

    test 3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello test 2 Node!\n'
+  const msg = 'Hello test 3 Node!\n'
   res.end(msg);
 });
 
